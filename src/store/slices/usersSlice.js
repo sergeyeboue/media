f928ecde5767da32df1b95e3f1bf2f5cb4384bf1
Serge Yeboue <serge.yeboue17@gmail.com>
2025-01-11 03:32:12 -0500

    upDate usesSlice for loading with redux and useState

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -5,37 +5,17 @@ import { addUser } from "../thunks/addUser";
 const usersSlice = createSlice({
   name: "users",
   initialState: {
-    isLoading: false,
     data: [],
-    error: null,
   },
   extraReducers(builder) {
     //reducer pour recuperer les users
-    builder.addCase(fetchUsers.pending, (state, action) => {
-      state.isLoading = true;
-      state.error = null;
-    });
     builder.addCase(fetchUsers.fulfilled, (state, action) => {
       state.data = action.payload;
-      state.isLoading = false;
-    });
-    builder.addCase(fetchUsers.rejected, (state, action) => {
-      state.isLoading = false;
-      state.error = action.error.message;
     });
 
     //reducer pour add user
-    builder.addCase(addUser.pending, (state, action) => {
-      // state.isLoading = true;
-      state.error = null;
-    });
     builder.addCase(addUser.fulfilled, (state, action) => {
-      state.isLoading = false;
       state.data.push(action.payload);
-    });
-    builder.addCase(addUser.rejected, (state, action) => {
-      state.isLoading = false;
-      state.error = action.error.message;
     });
   },
 });
