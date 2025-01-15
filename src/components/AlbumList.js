90840dc08be03d0f89d98b85a54ec58b49e682d5
Serge Yeboue <serge.yeboue17@gmail.com>
2025-01-15 18:15:39 -0500

    add redux toolkit query

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,4 +1,8 @@
+import { useFetchAlbumsQuery } from "../store";
+
 function AlbumList({ user }) {
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  console.log(data, error, isLoading);
   return <div>AlbumList For {user.name}</div>;
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { albumsApi } from "./apis/albumsApi";
 
 const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(albumsApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export { store };
 export * from "./thunks/fetchUsers";
 export * from "./thunks/addUser";
 export * from "./thunks/removeUser";
+export { useFetchAlbumsQuery } from "./apis/albumsApi";
